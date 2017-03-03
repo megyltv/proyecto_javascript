e769ad74a0f69064e33a348554554c38e0f936fd
deidy <deidamia.quishpe@gmail.com>
2017-03-03 13:18:24 -0500

    crearLibro component

--- Bookworms/aplicacion/src/app/nuevo-libro-admin/nuevo-libro-admin.component.ts
+++ Bookworms/aplicacion/src/app/nuevo-libro-admin/nuevo-libro-admin.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {MasterUrlService} from "../services/master-url.service";
+import {Response, Http} from "@angular/http";
 
 @Component({
   selector: 'app-nuevo-libro-admin',
@@ -7,9 +9,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NuevoLibroAdminComponent implements OnInit {
 
-  constructor() { }
+  title: string = "Bookworm's Review";
+  nuevoLibro = {};
+  libros = [];
+
+  disabledButtons = {
+    NuevoLibroFormSubmitButton: false
+  };
+
+  constructor(private _http: Http,
+              private _masterUrl: MasterUrlService) { }
 
   ngOnInit() {
+    this._http.get(this._masterUrl.url + "administrador/crearLibro")
+      .subscribe(
+        (res: Response) => {
+          this.libros = res.json()
+            .map((value) => {
+              value.formularioCerrado = true;
+              return value;
+            });
+        },
+        (err) => {
+          console.log(err);
+        }
+      )
+  }
+
+  crearLibro(formulario){
+    this.disabledButtons.NuevoLibroFormSubmitButton = true;
+    let libro = {
+      nombreLibro: formulario.value.nombreLibro,
+      autorLibro: formulario.value.autorLibro,
+      numeroPaginas: formulario.value.numeroPaginas,
+      categoriaLibro: formulario.value.categoriaLibro
+    }
+
+    this._http.post(this._masterUrl.url + "administrador/crearLibro", libro)
+      .suscribe(
+        (res)=>{
+          this.libros.push(res.json());
+          this.nuevoLibro = {};
+          this.disabledButtons.NuevoLibroFormSubmitButton = false;
+        },
+        (err)=>{
+          console.log("Ocurrio un error", err);
+        }
+      );
   }
 
 }
