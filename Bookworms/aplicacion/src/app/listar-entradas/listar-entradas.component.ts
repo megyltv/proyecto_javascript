1a3cf4ced7f5a70326beed38822c72010de48e77
a <a>
2017-03-06 13:13:28 -0500

    html

--- Bookworms/aplicacion/src/app/listar-entradas/listar-entradas.component.ts
+++ Bookworms/aplicacion/src/app/listar-entradas/listar-entradas.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
+import {Http, Response} from "@angular/http";
+import {MasterUrlService} from "../services/master-url.service";
 
 @Component({
   selector: 'app-listar-entradas',
@@ -7,9 +10,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListarEntradasComponent implements OnInit {
 
-  constructor() { }
+  title="Frases";
+  description="A continuacion se listan las frases ingresadas para el libro";
+  private _parametros:any;
+  resenias=[];
+
+  constructor(private _ActivateRoute:ActivatedRoute, private _http:Http, private _masterURL:MasterUrlService) { }
 
   ngOnInit() {
+    this._ActivateRoute.params.subscribe(parametros=>{
+      this._parametros=parametros;
+      console.log(parametros);
+      this._http.get(this._masterURL.url+'Resenias?idLibro='+this._parametros.idLibro).subscribe(
+        (res:Response)=>{
+          this.resenias=res.json().map((value)=>{
+            value.formularioCerrado=true;
+            return value;
+          });
+        },
+        (err)=>{
+          console.log(err);
+        }
+      )
+    })
+
   }
+
 
 }
--- Bookworms/aplicacion/src/app/nueva-entrada/nueva-entrada.component.ts
+++ Bookworms/aplicacion/src/app/nueva-entrada/nueva-entrada.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {Http, Response} from "@angular/http";
+import {MasterUrlService} from "../services/master-url.service";
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-nueva-entrada',
@@ -7,9 +10,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NuevaEntradaComponent implements OnInit {
 
-  constructor() { }
+  title="Nueva Resenia";
+  subtitle="Actualizar Resenia";
+  description="";
+  nuevaResenia = {};
+  private _parametros:any;
+  resenias:any;
+
+  disabledButtons = {
+    NuevaReseniaFormSubmitButton: false
+  };
+
+  constructor(private _ActivateRoute:ActivatedRoute,
+              private _http:Http,
+              private _masterUrl: MasterUrlService) { }
 
   ngOnInit() {
+    this._ActivateRoute.params.subscribe(parametros=>{
+      this._parametros=parametros;
+      console.log(parametros);
+    });
+    //this.resenias.formularioCerrado=true;
   }
+
+  crearResenia(formulario){
+    this.disabledButtons.NuevaReseniaFormSubmitButton = true;
+    let resenia = {
+      resenia: formulario.value.resenia,
+      calificacion: formulario.value.calificacion,
+      idLibro: this._parametros.idLibro
+    };
+
+    this._http.post(this._masterUrl.url + "Resenia", resenia)
+      .subscribe(
+        (res)=>{
+          this.resenias.push(res.json());
+          this.nuevaResenia = {};
+          this.disabledButtons.NuevaReseniaFormSubmitButton = false;
+        },
+        (err)=>{
+          console.log("Ocurrio un error", err);
+        }
+      );
+  }
+
 
 }
